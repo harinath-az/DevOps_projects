1f320be7b1ab1dbf479d695ba5e9e6844bcfe349
Harinath Reddy <hnarapur@uab.edu>
2025-01-05 22:16:42 -0600

    set-password

--- linux_projects/user_automation.sh
+++ linux_projects/user_automation.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-# Checking if users already exist and adding them if they don't
-if [ $# -gt 0]; then
+# Checking if the user already exists, if not adding the user
+if [ $# -gt 0 ]; then
     USER=$1
-    echo $USER
-    EXISTING_USER=$(cat /etc/passwd | grep -i -w $USER | cut -d ":" -f1)
-    if ['${USER}'=$'{EXISTING_USER}']; then
-        echo "user exists, please enter another user name"
+    echo "Checking for user: $USER"
+    if id "$USER" $ >/dev/null; then
+        echo "User named "$USER" already exists, try another username"
     else
-        echo "adding new user"
-        sudo useradd -m --shell /bin/bash
+        echo "Adding new user..."
+        if sudo useradd -m "$USER" --shell /bin/bash; then
+            echo "User "$USER" added successfully"
+
+            # Generate temporary password
+            SPECIAL_CHARACTER=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+            PSWRD="Atlasian@${RANDOM}${SPECIAL_CHARACTER}"
+            if echo "$USER":"$PSWRD" | sudo chpswd; then
+                echo "Password set successfully"
+                echo "The user "$USER"'s temporary password is ${PSWRD}"
+
+                # Force password change
+                sudo passwd -e "$USER"
+            else
+                echo "Failed to set the password for $USER."
+                exit 1
+            fi
+
+        else
+            echo "Failed to add user, please check the permissions or try again with different username"
+        fi
     fi
 
 else
-    echo "Please enter a valid parameter"
+    echo "enter valid parameter"
 fi
