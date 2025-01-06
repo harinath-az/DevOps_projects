c4db36b62c2d933e22e80cdacc935a507e05f90e
Harinath Reddy <hnarapur@uab.edu>
2025-01-05 20:53:47 -0600

    user-addition-by-checking

--- linux_projects/user_automation.sh
+++ linux_projects/user_automation.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# Checking if users already exist and adding them if they don't
+if [ $# -gt 0]; then
+    USER=$1
+    echo $USER
+    EXISTING_USER=$(cat /etc/passwd | grep -i -w $USER | cut -d ":" -f1)
+    if ['${USER}'=$'{EXISTING_USER}']; then
+        echo "user exists, please enter another user name"
+    else
+        echo "adding new user"
+        sudo useradd -m --shell /bin/bash
+    fi
 
+else
+    echo "Please enter a valid parameter"
+fi
